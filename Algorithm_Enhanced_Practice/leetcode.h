40b691c2547d424f888ac5539790a0da9fdbc706
Yaohui Huang <hyh5212@163.com>
2019-08-22 02:28:03 +0800

    121 days countdown

--- Algorithm_Enhanced_Practice/leetcode.h
+++ Algorithm_Enhanced_Practice/leetcode.h
@@ -4,12 +4,28 @@
 #include<queue>
 #include<iostream>
 #include<stdlib.h>
-
+#include<assert.h>
 using namespace std;
 
 
 class Solution {
 public:
+
+	/*11. Container With Most Water*/
+	//int maxArea(vector<int>& height) {
+	//	int i = 0;
+	//	int r = height.size()-1;
+	//	long long max = -1;
+	//	while (i<r)
+	//	{
+	//		long long area = (r - i)*min(height[r], height[i]);
+	//		if (area > max) max = area;
+	//		if (height[r] > height[i]) i++;
+	//		else r--;
+	//	}
+	//	return max;
+	//}
+
 	/*	26. Remove Duplicates from Sorted Array	*/
 	//int removeDuplicates(vector<int>& nums) {
 	//	int length = 0;
@@ -40,8 +56,38 @@ public:
 	//	return k;
 	//}
 
+	/*75. Sort Colors*/
+	//void sortColors(vector<int>& nums) {
+	//	const int n = 3;
+	//	int count[n] = { 0 };
+	//	for (int i = 0;i < nums.size();i++) {
+	//		count[nums[i]]++;
+	//	}
+	//	int index = 0;
+	//	for (int i = 0;i < n;i++) {
+	//		for (int j = 0;j < count[n];j++) nums[index++] = i;
+	//	}	
+	//}
+	//void sortColors(vector<int>& nums) {
+	//	int zero = -1;  //nums[0 .. zero] == 0
+	//	int two = nums.size(); // nums[two..-n-1] == 2
+	//	for (int i = 0;i < two;) {
+	//		if (nums[i] == 1)
+	//			i++;
+	//		else if (nums[i] == 2) {
+	//			two--;
+	//			swap(nums[i], nums[two]);
+	//		}
+	//		else {// nums[i] == 0
+	//			zero++;
+	//			swap(nums[zero], nums[i]);
+	//			i++;
+	//		}
+	//	}
+	//}
+
 	/*80. Remove Duplicates from Sorted Array II*/
-	int removeDuplicates(vector<int>& nums) {
+	/*int removeDuplicates(vector<int>& nums) {
 		int k = 0;
 		bool t = true;
 		int temp = 99999;
@@ -67,7 +113,60 @@ public:
 		}
 
 		return k;
-	}
+	}*/
+
+	/*88. Merge Sorted Array*/
+	/*
+
+	/*125. Valid Palindrome*/
+	//bool isPalindrome(string s) {
+	//	int p = 0;
+	//	int r = s.length() - 1;
+	//	while (p<r)
+	//	{
+	//		if ((s[p] >= 'A'&& s[p] <= 'Z') || (s[p] >= 'a'&&s[p] <= 'z') || (s[p] >= '0'&&s[p] <= '9')) {
+	//			if ((s[r] >= 'A'&& s[r] <= 'Z') || (s[r] >= 'a'&&s[r] <= 'z') || (s[r] >= '0'&&s[r] <= '9')) {
+	//				char c = s[p]<'A'?s[p]:(s[p] >= 'a' ? s[p] : s[p] - 'A' + 'a');
+	//				char k = s[r]<'A' ? s[r] : (s[r] >= 'a' ? s[r] : s[r] - 'A' + 'a');
+	//				if (c != k) {
+	//					return false;
+	//				}
+	//				else {
+	//					p++;
+	//					r--;
+	//				}
+	//			}
+	//			else {
+	//				r--;
+	//				continue;
+	//			}
+	//		}
+	//		else {
+	//			p++;
+	//		}
+	//	}
+	//	return true;
+	//}
+
+	/*167. Two Sum II - Input array is sorted*/
+	//vector<int> twoSum(vector<int>& numbers, int target) {
+	//	
+	//	assert(numbers.size() >= 2);
+	//	int l = 0, r = numbers.size() - 1;
+	//	while (l < r)
+	//	{
+	//		if (numbers[l] + numbers[r] == target) {
+	//			int res[2] = { l + 2, r + 1 };
+	//			return vector<int>(res, res + 2);
+	//		}
+	//		else if (numbers[l] + numbers[r] < target) l++;
+	//		else r--; //numbers[l] + numbers[r] > target
+	//	}
+
+	//	throw invalid_argument("The input has no solution.");
+	//}
+
+
 	/*	283. Move Zeroes	*/
 	
 	//void moveZeroes(vector<int>& nums) {
@@ -97,8 +196,36 @@ public:
 	//	}
 	//}
 
+	/*344. Reverse String*/
+	//void reverseString(vector<char>& s) {
+	//	int p = 0;
+	//	int r = s.size()-1;
+	//	while (p < r)
+	//	{
+	//		swap(s[p++], s[r--]);
 
-	//
+	//	}
+	//}
+
+	/*345. Reverse Vowels of a String*/
+	//string reverseVowels(string s) {
+	//	int p = 0;
+	//	int r = s.length()-1;
+	//	while (p < r)
+	//	{	
+	//		if (s[p] == 'a' || s[p] == 'A' || s[p] == 'e' || s[p] == 'E' || s[p] == 'i' || s[p] == 'I' || s[p] == 'o' || s[p] == 'O'
+	//			|| s[p] == 'u' || s[p] == 'U')
+	//			if (s[r] == 'a' || s[r] == 'A' || s[r] == 'e' || s[r] == 'E' || s[r] == 'i' || s[r] == 'I' || s[r] == 'o' || s[r] == 'O'
+	//				|| s[r] == 'u' || s[r] == 'U')
+	//				swap(s[p++], s[r--]);
+	//			else
+	//				r--;
+	//		else
+	//			p++;
+
+	//	}
+	//	return s;
+	//}
 };
 
 #endif // !LEETCODE
--- Algorithm_Enhanced_Practice/main.cpp
+++ Algorithm_Enhanced_Practice/main.cpp
@@ -1,14 +1,18 @@
 #include<iostream>
 #include<vector>
+#include<string>
 #include"leetcode.h"
 using namespace std;
 
 int main() {
-	int arr[] = { 0,0,1,1,1,1,2,3,3 };
-	vector<int> vec(arr, arr + sizeof(arr) / sizeof(int));
-	cout << Solution().removeDuplicates(vec);
-	
-	for (int i = 0;i < vec.size();i++)
-		cout << vec[i] << " ";
-	cout << endl;
+	int nums1[] = { 1,8,6,2,5,4,8,3,7 };
+	int m = 3;
+	int nums2[] = {2, 5, 6};
+	int n =3;
+	vector<int> arr1(nums1, nums1 + sizeof(nums1)/sizeof(nums1[0]));
+	vector<int> arr2(nums2, nums2 + 3);
+
+	string s = "A man, a plan, a canal: Panama";
+	string s1 = "0p";
+	cout<< Solution().maxArea(arr1);
 }
